e81683314ab445a7fc472fa39d2576945a2ab0e1
Ben Hur <792704-benhur07b@users.noreply.gitlab.com>
2019-07-24 07:23:16 +0800

    Initial commit.

---
+++ static/js/main-leaflet.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+
+    var osm_mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    	maxZoom: 19,
+    	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })
+
+    var barangays = new L.GeoJSON.AJAX("data/barangays.geojson", {
+        style: {
+            fillColor: "#ffffff",
+            fillOpacity: 0.0,
+            color: '#000000',
+            weight: 1,
+        }
+    });
+    var tsunami = new L.GeoJSON.AJAX("data/tsunami.geojson", {
+        style: {
+            fillOpacity: 0.7,
+            weight: 1,
+        }
+    });
+    var building_footprints = new L.GeoJSON.AJAX("data/building-footprints.geojson", {
+        style: {
+            fillColor: "#ff0000",
+            fillOpacity: 0.9,
+            color: '#ff0000',
+            weight: 1,
+        }
+    });
+
+    barangays
+
+    var basemaps = {
+        "OSM Mapnik": osm_mapnik,
+    }
+
+    var overlays = {
+        "Barangays": barangays,
+        "Tsunami": tsunami,
+        "Building Footprints": building_footprints,
+    }
+
+    var map = L.map('map', {
+        center: [9.830, 118.745],
+        zoom: 10,
+        layers: [osm_mapnik, barangays, tsunami, building_footprints]
+    });
+
+    L.control.layers(basemaps, overlays).addTo(map);
+
+})
---
+++ static/js/main-mapbox.js
@@ -0,0 +1,113 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibmFzc2FjYXJpdGFzIiwiYSI6ImNqa2FweGhqcTF2dm4zd24xa2w0c3pzNDkifQ.IIEkXeNO8hhuQZu-Mw7Frg';
+
+$(document).ready(function(){
+
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v10',
+        center: [118.745, 9.830],
+        zoom: 9,
+        minZoom: 7,
+        maxZoom: 18,
+        // maxBounds: [[124.95, 11.03], [125.05, 11.08]]
+    });
+
+    map.addControl(new mapboxgl.NavigationControl(), 'top-left');
+
+    map.on('load', function () {
+
+        map.addSource(
+            'tsunami', {
+                type: 'geojson',
+                data: 'data/tsunami.geojson',
+            }
+        );
+
+        map.addSource(
+            'barangays', {
+                type: 'geojson',
+                data: 'data/barangays.geojson',
+            }
+        );
+
+        map.addSource(
+            'building-footprints', {
+                type: 'geojson',
+                data: 'data/building-footprints.geojson',
+            }
+        );
+
+        map.addLayer({
+            'id': 'tsunami_puerto-princesa',
+            'type': 'fill',
+            'source': 'tsunami',
+            'layout': {
+                'visibility': 'visible'
+            },
+            'paint': {
+                'fill-color': 'blue',
+                'fill-outline-color': 'blue',
+            }
+        });
+
+        map.addLayer({
+            'id': 'barangays_puerto-princesa',
+            'type': 'line',
+            'source': 'barangays',
+            'layout': {
+                'visibility': 'visible'
+            },
+            'paint': {
+                'line-color': 'black',
+            }
+        });
+
+        map.addLayer({
+            'id': 'building-footprints_puerto-princesa',
+            'type': 'fill-extrusion',
+            'source': 'building-footprints',
+            'layout': {
+                'visibility': 'visible'
+            },
+            'paint': {
+                // 'fill-extrusion-color': 'red',
+                'fill-extrusion-color': 'red',
+                'fill-extrusion-opacity': 0.75,
+                'fill-extrusion-height': 20,
+            },
+        });
+
+
+    });
+
+    var toggleableLayerIds = [ 'Barangays', 'Tsunami', 'Building Footprints' ];
+
+    for (var i = 0; i < toggleableLayerIds.length; i++) {
+        var id = toggleableLayerIds[i];
+
+        var link = document.createElement('a');
+        link.href = '#';
+        link.className = 'active';
+        link.textContent = id;
+
+        link.onclick = function (e) {
+        var clickedLayer = this.textContent.split(" ").join("-").toLowerCase() + '_puerto-princesa';
+        e.preventDefault();
+        e.stopPropagation();
+
+        var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+
+        if (visibility === 'visible') {
+        map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+        this.className = '';
+        } else {
+        this.className = 'active';
+        map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+        }
+        };
+
+        var layers = document.getElementById('menu');
+        layers.appendChild(link);
+    }
+
+});
